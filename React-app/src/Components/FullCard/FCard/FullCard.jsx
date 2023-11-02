7ab870cd096ad196724394c81f5685608fd77051
PACode2121 <code@pakistanautomation.com.pk>
2023-11-02 15:38:30 +0500

    format changing in Fullcard.jsx file

--- React-app/src/Components/FullCard/FCard/FullCard.jsx
+++ React-app/src/Components/FullCard/FCard/FullCard.jsx
@@ -5,18 +5,15 @@ import Object from "./FullCard.module.css";
 const FullCard = (props) => {
   return (
     // <div className="col-md-3 mb-4">
-      <Card
-        className={Object.card}
-        style={{ backgroundColor: props.backColor}}
-      >
-        <Card.Body>
-          <div className={Object.circle}>
-            <span className={Object.icon}>{props.icon}</span>
-          </div>
-          <Card.Title className={Object.title}>{props.title}</Card.Title>
-          <Card.Text>{props.desc}</Card.Text>
-        </Card.Body>
-      </Card>
+    <Card className={Object.card} style={{ backgroundColor: props.backColor }}>
+      <Card.Body>
+        <div className={Object.circle}>
+          <span className={Object.icon}>{props.icon}</span>
+        </div>
+        <Card.Title className={Object.title}>{props.title}</Card.Title>
+        <Card.Text>{props.desc}</Card.Text>
+      </Card.Body>
+    </Card>
     // </div>
   );
 };
